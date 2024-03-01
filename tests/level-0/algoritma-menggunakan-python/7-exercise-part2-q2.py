83d7c18319d37d14f6922f5b5569833abf717e39
Rahmanda Wibowo <rahmandawibowo@gmail.com>
2024-03-01 17:56:37 +0100

    feat: add test for exercise part 2 q2

---
+++ tests/level-0/algoritma-menggunakan-python/7-exercise-part2-q2.py
@@ -0,0 +1,36 @@
+import io
+import unittest
+from unittest.mock import patch
+from challenge import main
+
+
+class TestExercisePart1(unittest.TestCase):
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_one(self, stdout):
+        main(4)
+        expected_out =  \
+"""   *
+  ***
+ *****
+*******
+"""
+        self.assertEqual(stdout.getvalue(), expected_out)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_two(self, stdout):
+        main(2)
+        expected_out = \
+""" *
+***
+"""
+        self.assertEqual(stdout.getvalue(), expected_out)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_three(self, stdout):
+        main(1)
+        expected_out = '*\n'
+        self.assertEqual(stdout.getvalue(), expected_out)
+
+if __name__ == '__main__':
+    unittest.main(exit=False)
