04d35724ffce58b5b392adca53fb87395bcec8c6
Rahmanda Wibowo <rahmandawibowo@gmail.com>
2024-03-07 09:45:29 +0100

    feat: add test for exercise part 2 q2

--- tests/level-0/algoritma-menggunakan-python/7-exercise-part2-q3.py
+++ tests/level-0/algoritma-menggunakan-python/7-exercise-part2-q3.py
@@ -8,41 +8,28 @@ class TestExercisePart2(unittest.TestCase):
 
     @patch('sys.stdout', new_callable=io.StringIO)
     def test_one(self, stdout):
-        main(7)
+        main(4)
         expected_out =  \
-"""*
-**
-***
-****
-***
-**
-*
+"""   *
+  ***
+ *****
+*******
 """
         self.assertEqual(stdout.getvalue(), expected_out)
 
     @patch('sys.stdout', new_callable=io.StringIO)
     def test_two(self, stdout):
-        main(6)
-        expected_out =  \
-"""*
-**
-***
-***
-**
-*
-"""
-        self.assertEqual(stdout.getvalue(), expected_out)
-    
-    @patch('sys.stdout', new_callable=io.StringIO)
-    def test_three(self, stdout):
         main(2)
-        expected_out =  '*\n*\n'
+        expected_out = \
+""" *
+***
+"""
         self.assertEqual(stdout.getvalue(), expected_out)
 
     @patch('sys.stdout', new_callable=io.StringIO)
-    def test_four(self, stdout):
+    def test_three(self, stdout):
         main(1)
-        expected_out =  '*\n'
+        expected_out = '*\n'
         self.assertEqual(stdout.getvalue(), expected_out)
 
 if __name__ == '__main__':
